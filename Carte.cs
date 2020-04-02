8e2db8a537e828892b0310b856f8555e055d64f7
Claudiu1999 <63072414+Claudiu1999@users.noreply.github.com>
2020-04-02 22:30:21 +0300

    Add files via upload

---
+++ Carte.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bilblioteca
+{
+    enum Categorie { Nuvela, Poezie, Roman, Comedie };
+    class Carte
+    {
+        string nume;
+        string autor;
+        string editura;
+        int nrexemplare;
+        public Categorie categorie;
+        public string Nume { get; set; }
+        public string Autor { get; set; }
+        public string Editura { get; set; }
+
+        public int NrExemplare { get; set; }
+
+
+        public Carte()
+        {
+            nume = string.Empty;
+            autor = string.Empty;
+            editura = string.Empty;
+            nrexemplare = 0;
+        }
+
+        public Carte(string _nume, string _autor, string _editura, int _nrexemplare)
+        {
+            nume = _nume;
+            autor = _autor;
+            editura = _editura;
+            nrexemplare = _nrexemplare;
+            
+        }
+
+        public Carte(string _nume, string _autor, string _editura, int _nrexemplare, Categorie _categorie)
+        {
+            nume = _nume;
+            autor = _autor;
+            editura = _editura;
+            nrexemplare = _nrexemplare;
+            categorie = _categorie;
+
+        }
+
+        public Carte(String nume1)
+        {
+            string[] buff= nume.Split(',');
+            nume = buff[0];
+            autor = buff[1];
+            editura = buff[2];
+            nrexemplare = Convert.ToInt32( buff[3] );
+
+        }
+
+        public string ComparaExemplare(Carte c1,Carte c2)
+        {
+            if (c1.nrexemplare > c2.nrexemplare)
+                return string.Format("Cartea {0} are mai multe exemplare", c1.nume);
+            else
+                if (c1.nrexemplare == c2.nrexemplare)
+                    return "Ambele carti au acelasi numar de exemplare";
+                else
+                    return string.Format("Cartea {0} are mai multe exemplare", c2.nume);
+        }
+
+        public string Info()
+        {
+            return string.Format(" Cartea se numeste {0}, scrisa de autorul {1}, de la editura {2}, fiind {3} exemplare, si face parte din categorie {4}", nume, autor, editura,nrexemplare,categorie);
+        }
+
+        public string ConversieLaSir()
+        {
+            string sir;
+            sir = string.Format(" Cartea se numeste {0}, scrisa de autorul {1}, de la editura {2}, fiind {3} exemplare", nume, autor, editura, nrexemplare);
+            return sir;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Bilblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Carte();
+            string s = c.Info();
+            Console.WriteLine(s);
+
+
+            Carte c1 = new Carte("Ion", "Liviu Rebreanu", "MINERVA",10);
+            string s1 = c1.ConversieLaSir();
+            Console.WriteLine(s1);
+            
+
+            var c2 = new Carte("Luceafarul", "Mihai Eminescu", "CANONICA",20,Categorie.Poezie);
+            string s2 = c2.Info();
+            Console.WriteLine(s2);
+            string s3 = c1.ComparaExemplare(c1,c2);
+
+            Carte c3 = new Carte("O scrisoare pierduta", "Ion Luca Caragiale", "REGIS", 15, Categorie.Comedie);
+            Console.WriteLine(c3.Info());
+            Console.ReadKey();  
+        }
+
+        public static Carte CitireCarteTastatura()
+        {
+            Console.WriteLine("Introduceti numele cartii : ");
+            string nume = Console.ReadLine();
+
+            Console.WriteLine("Introduceti autorul cartii : ");
+            string autor = Console.ReadLine();
+
+            Console.WriteLine("Introduceti editura cartii : ");
+            string editura = Console.ReadLine();
+
+            Console.WriteLine("Introduceti numarul de exemplare:");
+            int nrexemplare = Convert.ToInt32( Console.ReadLine() );
+
+
+            Carte c3 = new Carte(nume, autor, editura,nrexemplare);
+            return c3;
+        }
+    }
+}
